33ba5ecd3186ceb10d6732624127b90359a7c8f9
Jose Rivas <jose.rivas.0410@gmail.com>
2021-04-09 14:50:56 -0500

    created the messages database table and model and put in some data inside of it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save { self.username = username.downcase }
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 15 }
+  has_many :messages
   has_secure_password
 end
