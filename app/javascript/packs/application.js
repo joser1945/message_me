87c920f429f869fd1e980fe78236343accd85f9c
Jose Rivas <jose.rivas.0410@gmail.com>
2021-04-13 14:21:00 -0500

    did not finish because of the input errors for not clearing up input field

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,12 +18,7 @@ $(document).on('turbolinks:load', function() {
     $('.message .close').on('click', function() {
         $(this).closest('.message').transition('fade');
     });
-    
-    $("#message_body").bind("ajax:complete", function(event,xhr,status){
-        $('#message_body').val('');
-    });
 
-    submit_message();
     scroll_bottom();
 
 })
@@ -33,12 +28,3 @@ scroll_bottom = $('#message_box').ready(function() {
         $('#message_box').scrollTop($('#message_box')[0].scrollHeight);
     };
 });
-
-submit_message = function() {
-    $('#message_body').on('keydown', function(e) {
-      if (e.keyCode == 13) {
-        $('button').click();
-        e.target.value = "";
-      };
-    });
-  };
